0fbe71d31c8f1fe48047048fe87546cb3220dad7
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-03-30 16:41:53 +0100

    root page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,6 +2,9 @@
 class QuotesController < Seneca::Controller
   def day_quote
   "  90% of everything is crap.
-- Theodore Sturgeon "
+- Theodore Sturgeon " + "\n<pre>\n#{env}\n</pre>"
+  end
+  def exception
+    raise 'No data available'
   end
 end
